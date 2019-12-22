a7e32a99bd35b85bc9461ed827cae2d9b320eafb
Aleksandr <asavostin1@gmail.com>
2019-12-23 02:36:07 +0500

    На проверку

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,53 @@
 'use strict';
+
+(function () {
+var CLASS_HIDDEN = 'visually-hidden';
+var ESC_KEY_CODE = 27;
+var popup = document.querySelector('.popup');
+var closePopup = document.querySelector('.popup__close');
+var openPopup = document.querySelector('.header__contact-callback');
+var popupBackground = document.querySelector('.header__contact-callback');
+
+var delClass = function (nameElement) {
+  nameElement.classList.remove(CLASS_HIDDEN);
+};
+
+var addClass = function (nameElement) {
+  nameElement.classList.add(CLASS_HIDDEN);
+};
+
+if (openPopup) {
+	openPopup.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		delClass(popup);
+	});
+}
+
+if (closePopup) {
+	closePopup.addEventListener('click', function(evt) {
+		addClass(popup);
+	});
+}
+
+if (popupBackground) {
+	popupBackground.addEventListener('click', function(evt) {
+		addClass(popup);
+	});
+}
+
+if (popup) {
+	document.addEventListener('keydown', function(evt) {
+		if (evt.keyCode === ESC_KEY_CODE) {
+			addClass(popup);
+		}
+	});
+}
+
+
+
+
+
+
+
+
+})();
